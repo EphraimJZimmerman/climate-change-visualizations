87bd480dd73b23b727a9d4cc5994cb44cfcfc349
EphraimJZimmerman <68978246+EphraimJZimmerman@users.noreply.github.com>
2022-02-16 22:53:17 -0500

    Updated data template

---
+++ Data/data_loop.py
@@ -0,0 +1,19 @@
+
+n = 1
+
+print("[", end="")
+for i in range(1922, 2023, 10): 
+   
+    print(i, end="")
+    print("%02d" % (n,), end="")
+    print("%02d" % (n,), end="")
+    print(", ", end="")
+
+print("]")
+
+# I did it this way so I don't have to deal with arrays. Just delete the last comma at the end. 
+
+arr = []
+for i in range(10):
+    arr.append("null")
+print(arr)
