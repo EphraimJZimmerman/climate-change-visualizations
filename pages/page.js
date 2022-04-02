3e0e6b7094d00136808bd52fd74ae08806eb722d
EphraimJZimmerman <68978246+EphraimJZimmerman@users.noreply.github.com>
2022-04-02 16:38:33 -0400

    Added charts example

--- pages/page.js
+++ pages/page.js
@@ -5,6 +5,9 @@ import Image from 'next/image'
 import Link from "next/link";
 import Head from "next/head";
 
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Chart }            from 'react-chartjs-2'
+import {Line} from 'react-chartjs-2';
 
 /*
 TASK:
@@ -17,9 +20,41 @@ DATA VISUALIZATIONS for Week of March 28th, 2022
 - Create exception handling for missing data and slow retrieval times (loading/error messages, etc.)
 - You can view the live website from https://climate-change-visualizations.vercel.app/ on your computer or phone
 
+
+npm install --save react-chartjs-2 chart.js
+
 */
 
+const chartData = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40]
+    }
+  ]
+};
+
 export default function page({ data }) {
+
+
 
   let data1 = data[0][0]
 
@@ -46,6 +81,8 @@ export default function page({ data }) {
     heat += toDate(dateRange[i]) + ": " + historical[i] + " | "
   }
   console.log(heat)
+
+
  
   return (<>
     
@@ -55,32 +92,16 @@ export default function page({ data }) {
       <script src="https://code.jscharting.com/2.9.0/jscharting.js"></script>
     </Head>
 
-    {/* Curly braces '{}' mean it is JavaScript inside of HTML. 
-    For the following, exampleVar is a variable created above. */}
-
-
-    {/*  Add as many paragraphs or titles as necessary.*/}
-
     <h2>Data for {title}.</h2>
     <h3>Last updated {"lastUpdated"}</h3>
+      
+    <Line
+      data={chartData}
+      width={400}
+      height={400}
+    />
     
-    
-    <script  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">     </script>
-    
-    <body>
-    <canvas id="myChart" style="width:100%;max-width:700px"></canvas>
-    
-    <script>
-    {var myChart = new Chart("myChart", {
-      type: "line",
-      data: {},
-      options: {}
-    })};
-    </script>
-
-    </body>
-    
-
+ 
   </>)
 }
 
